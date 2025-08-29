c2725afedfa1195960d11ad56c9850bbf2d2f112
tszhong0411 <me@nelsonlai.me>
2025-08-29 13:57:28 +0800

    fix(eslint-config): disable more rules that are handled by TS

--- packages/eslint-config/src/configs/node.ts
+++ packages/eslint-config/src/configs/node.ts
@@ -14,6 +14,11 @@ export const node: Linter.Config[] = [
       // Handled by TypeScript
       'node/no-missing-import': 'off',
       'node/no-missing-require': 'off',
+      'node/no-unpublished-bin': 'off',
+      'node/no-unpublished-import': 'off',
+      'node/no-unpublished-require': 'off',
+      'node/no-unsupported-features/es-builtins': 'off',
+      'node/no-unsupported-features/es-syntax': 'off',
       'node/no-unsupported-features/node-builtins': 'off'
     }
   }
