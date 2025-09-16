75f2f1d5b3cb8088a3c1da1705349d64493782f6
Nelson Lai <me@nelsonlai.dev>
2025-09-16 22:37:21 +0800

    refactor: update comments to block style

--- packages/eslint-config/src/global.d.ts
+++ packages/eslint-config/src/global.d.ts
@@ -1,4 +1,4 @@
-/**
+/*
  * This declaration file defines the missing/incompatible types for ESLint plugins.
  * Note: only the types I needed are defined.
  *
@@ -93,8 +93,10 @@ declare module '@typescript-eslint/eslint-plugin' {
   const plugin: ESLint.Plugin & {
     configs: {
       'eslint-recommended': {
-        // Defined
-        // https://github.com/typescript-eslint/typescript-eslint/blob/264ca2fad49a298a2963de83746f8e8044229a3b/packages/eslint-plugin/src/configs/eslint-recommended-raw.ts
+        /*
+         * Defined
+         * https://github.com/typescript-eslint/typescript-eslint/blob/264ca2fad49a298a2963de83746f8e8044229a3b/packages/eslint-plugin/src/configs/eslint-recommended-raw.ts
+         */
         overrides: [
           {
             files: string[]
