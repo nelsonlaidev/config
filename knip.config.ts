0ac64dded5bdbf7b48e87042568bd81550bb4fd3
Nelson Lai <me@nelsonlai.dev>
2026-03-17 00:27:25 +0800

    fix(knip): add ignore pattern for eslint-plugin tests

--- knip.config.ts
+++ knip.config.ts
@@ -5,6 +5,9 @@ const config: KnipConfig = {
     'packages/eslint-config': {
       entry: ['eslint-inspector.config.ts', 'src/index.ts'],
     },
+    'packages/eslint-plugin': {
+      ignore: ['tests/**/*.ts'],
+    },
   },
 }
 
