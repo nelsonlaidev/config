1ca1f11dea3d42a1975238f103fa32a3723a0381
Nelson Lai <me@nelsonlai.dev>
2026-03-21 16:03:54 +0800

    fix(oxlint-config): use standard glob patterns

--- packages/oxlint-config/src/globs.ts
+++ packages/oxlint-config/src/globs.ts
@@ -1,8 +1,8 @@
-export const GLOB_SRC_EXT = '?([cm])[jt]s?(x)'
-export const GLOB_SRC = '**/*.?([cm])[jt]s?(x)'
+export const GLOB_SRC_EXT = '{js,jsx,ts,tsx,cjs,cts,mjs,mts}'
+export const GLOB_SRC = `**/*.${GLOB_SRC_EXT}`
 
-export const GLOB_JS = '**/*.?([cm])js'
-export const GLOB_JSX = '**/*.?([cm])jsx'
+export const GLOB_JS = '**/*.{js,cjs,mjs}'
+export const GLOB_JSX = '**/*.{jsx,cjsx,mjsx}'
 
-export const GLOB_TS = '**/*.?([cm])ts'
-export const GLOB_TSX = '**/*.?([cm])tsx'
+export const GLOB_TS = '**/*.{ts,cts,mts}'
+export const GLOB_TSX = '**/*.{tsx,ctsx,mtsx}'
