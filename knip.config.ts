b3af9068074510f4a93b571c11dec49665cb5104
Nelson Lai <me@nelsonlai.dev>
2026-03-29 00:05:39 +0800

    chore: update knip

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
-  ignoreDependencies: [/^(@[\w-]+\/)?eslint-plugin(-[\w-]+)?$/],
+  ignoreDependencies: [/eslint-plugin/],
 }
 
 export default config
