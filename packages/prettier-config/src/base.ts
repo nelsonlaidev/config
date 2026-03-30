abba48675b8d6bb30be69379647ccebb8ce32480
Nelson Lai <me@nelsonlai.dev>
2026-03-30 15:33:33 +0800

    chore(prettier-config): restore the package

---
+++ packages/prettier-config/src/base.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'prettier'
+
+export const defineConfig = (config: Config = {}): Config => {
+  const { plugins = [], ...rest } = config
+
+  return {
+    arrowParens: 'always',
+    singleQuote: true,
+    jsxSingleQuote: true,
+    tabWidth: 2,
+    semi: false,
+    trailingComma: 'all',
+    endOfLine: 'lf',
+    plugins: ['prettier-plugin-packagejson', ...plugins],
+    printWidth: 120,
+    ...rest,
+  }
+}
