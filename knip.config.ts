f9c4ec14c81840107a8af98ebe7ffee3c2ab828f
Nelson Lai <me@nelsonlai.dev>
2026-03-30 15:38:28 +0800

    chore: fix knip

--- knip.config.ts
+++ knip.config.ts
@@ -2,6 +2,15 @@ import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
   ignoreDependencies: [/eslint-plugin/],
+  workspaces: {
+    'packages/eslint-config': {
+      entry: ['eslint-inspector.config.ts', 'src/index.ts'],
+      ignoreDependencies: [
+        // Required by eslint-plugin-import-x
+        'eslint-import-resolver-typescript',
+      ],
+    },
+  },
 }
 
 export default config
