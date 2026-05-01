a4f0df4b57663e379348c2a2ff1af7a959d2aa71
Nelson Lai <me@nelsonlai.dev>
2026-05-01 20:12:05 +0800

    fix(oxlint-config): omit `prefer-regex-literals` from regexp recommended rules

---
+++ .changeset/tender-donkeys-peel.md
@@ -0,0 +1,5 @@
+---
+'@nelsonlaidev/oxlint-config': patch
+---
+
+Omit `prefer-regex-literals` from regexp recommended rules (not supported by Oxlint)
--- packages/oxlint-config/src/configs/regexp.ts
+++ packages/oxlint-config/src/configs/regexp.ts
@@ -1,5 +1,7 @@
 import type { OxlintOverride } from 'oxlint'
 
+import { omit } from 'es-toolkit/object'
+
 import { GLOB_SRC } from '../globs'
 import { regexpPlugin } from '../plugins'
 
@@ -8,7 +10,8 @@ export const regexp = (): OxlintOverride[] => [
     files: [GLOB_SRC],
     jsPlugins: ['eslint-plugin-regexp'],
     rules: {
-      ...regexpPlugin.configs.recommended.rules,
+      // Recommended rules enable some rules that are not supported by Oxlint yet.
+      ...omit(regexpPlugin.configs.recommended.rules, ['prefer-regex-literals']),
     },
   },
 ]
