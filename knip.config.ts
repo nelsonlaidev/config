4039d24e2ae3a27993fe6ec1a0311efec186d61e
Nelson Lai <me@nelsonlai.dev>
2026-05-01 20:16:48 +0800

    fix: knip

--- knip.config.ts
+++ knip.config.ts
@@ -1,10 +1,13 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
-  ignoreDependencies: [/eslint-plugin/, /prettier-plugin/],
+  ignoreDependencies: [/eslint-plugin/],
   workspaces: {
     'packages/eslint-config': {
       entry: ['eslint-inspector.config.ts', 'src/index.ts'],
+    },
+    'packages/oxlint-config': {
+      ignoreFiles: ['tests/fixtures/**/*'],
     },
   },
 }
